a8299954ce42c469dec31fc04f1adec7eab723df
--replace-all <visolad1@gmail.com>
2023-12-05 21:25:44 +0500

    lesson 19

---
+++ Lesson_19/index.js
@@ -0,0 +1,140 @@
+// function test() {
+//     var a = "Hello world"
+//     console.log(a)
+// }
+
+// let a = 'const';
+
+// a = 'hhh';
+
+// console.log(a)
+
+// if (true) {
+//     var b = "Hi"
+//     console.log(b)
+// }
+
+// console.log(b)
+
+// test()
+
+
+// number, string, boolean, undefined, object.
+
+// number - числовые значения
+// string - строки
+// boolean - булевые значения правда(true) или неправда(false) 
+// undefined - означает что переменная не имеет значения
+// object - объекты
+
+// null - указывает на пустой объект.
+
+// let a;
+
+// if (false) {
+//     a = 'string'
+// }
+
+// console.log(a)
+
+// let num = 1333
+// num = 0.5
+
+// let str = "", '', ``
+// console.log(`Hello ${num * 3}`)
+
+// let abc;
+// console.log(abc) == undefined
+
+// if (true) {
+//     console.log(a) === undefined
+//     var a = 15;
+//     console.log(a) === 15
+// }
+
+// NaN - not a number
+
+// let str = '15la'
+// console.log(typeof str)
+
+// str = Number(str)
+// console.log(str)
+// console.log(typeof str)
+
+// if (NaN === 15) {
+//     console.log('hi')
+// } else {
+//     console.log("Hello")
+// }
+
+// let b = -Number.MAX_VALUE * 2 // -INFINITY
+
+// console.log(b)
+
+// let i = 13
+// i--
+// i = i + 1
+// i = i - 1
+// console.log(i)
+
+// +, -, *, /
+// %
+
+// ==, ===
+// let a = '15'
+// let b = 15
+// if (a === b) {
+//     console.log("True")
+// } else {
+//     console.log("False")
+// }
+
+// !=, !== 
+// >=, <=
+// >, <
+
+// a = '15'
+// b = 15
+
+// if (a !== b) {
+//     console.log('True')
+// } else {
+//     console.log('False')
+// }
+
+
+// let monthNumber = 7
+
+// if (monthNumber === 1 || monthNumber === 2 || monthNumber === 12) {
+
+//     console.log("Зима");
+
+// } else if (monthNumber >= 3 && monthNumber <= 5) {
+
+//     console.log("Весна");
+
+// } else if (monthNumber >= 6 && monthNumber <= 8) {
+
+//     console.log("Лето");
+
+// } else if (monthNumber >= 9 && monthNumber <= 11) {
+
+//     console.log("Осень");
+
+// } else {
+
+//     console.log("Введите корректный номер месяца");
+// }
+
+
+// let number = 227
+
+// if (number % 10 === 5) {
+//     console.log("Число оканчивается на 5");
+// }
+// else {
+//     console.log("Число не оканчивается на 5");
+// }
+
+// && - and
+// || - or
