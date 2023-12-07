4b068faf07cf0cb003a87074601ff3f67e2c79f6
--replace-all <visolad1@gmail.com>
2023-12-07 16:25:18 +0500

    HW

---
+++ Lesson_20/index.js
@@ -0,0 +1,27 @@
+// let str1 = "Введите длину автомобиля"
+// let str2 = "Введите объем двигателя"
+// let length = 3.5
+// let engine = 1.1
+
+// if (length <= 3.8 && engine <= 1.2) {
+//     console.log("Class A")
+// } else if (length <= 4 && engine <= 1.6) {
+//     console.log("Class B")
+// } else if (length <= 4.5 && engine == 1.6 && engine <= 2) {
+//     console.log("Class C")
+// } else {
+//     console.log("Выберите автомобиль другого класса")
+// }
+
+
+// let str = 'Введите текущий час'
+// let hour = 19
+
+// if (hour <= 11) {
+//     console.log("Доброе утро")
+// } else if (hour >= 12 <= 17) {  !! (hour >= 12 && hour <=17 )
+//     console.log("Добрый день")
+// } else if (hour >= 18 <= 23) {  !! ( hour >= 18 && hour <= 23 )
+//     console.log("Добрый вечер")
+
+// }
