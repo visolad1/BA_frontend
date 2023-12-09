2a92030c2fe521a71a508ae2013fc10b4931fe4f
--replace-all <visolad1@gmail.com>
2023-12-09 18:11:08 +0500

    Lesson 21

--- Lesson_20/index.js
+++ Lesson_20/index.js
@@ -25,3 +25,37 @@
 //     console.log("Добрый вечер")
 
 // }
+
+// number string boolean object symbol undefined
+
+// 1, 1.3, NaN
+// "" '' ``
+// true false
+
+// ===, ==
+
+// let a = 15
+// let b = '15'
+// if (a === b) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// >, <, =, !, >=, <=, !=, !==
+
+// var
+// let
+// const c = 1;
+// c = 2 !!!!!!11
+
+// if (true) {
+//     var varVariable = 3
+//     let letVariable = 4
+//     console.log(varVariable, letVariable)
+// }
+
+// console.log(letVariable) // !!!!!!!!!!!!
+
+// console.log('Hello world \n Hi') // перевод строки
+// console.log('Hello world \t Hi') // табуляция
---
+++ Lesson_21/index.js
@@ -0,0 +1,31 @@
+if (false) {
+    console.log("True")
+} else {
+    console.log("False")
+}
+
+let a = true
+
+a === true ? console.log('True') : console.log('False')
+a ? console.log('True') : console.log('False')
+
+// условие ? значение если правда : значение если ложь
+
+let age = 19
+
+age >= 18 ? console.log("Совершеннолетний") : console.log("Несовершеннолетний")
+
+let b = true
+
+b ? console.log('True') : console.log('False')
+
+let day = 6
+
+// day === 7 || day === 6 ? console.log("v") : console.log("r")
+
+day > 7 ? console.log('error') : day < 6 ? console.log('Рабочий день') : console.log('Выходной')
+
+let done = 5
+let chores = 5
+
+done === chores ? console.log("All done") : console.log("All havent done")
