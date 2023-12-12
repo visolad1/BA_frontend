cc0f87496d78f008942402c2535b40f930a17009
--replace-all <visolad1@gmail.com>
2023-12-12 19:41:23 +0500

    Lesson 22

---
+++ Lesson_22/index.js
@@ -0,0 +1,68 @@
+// let month = 12
+// let degrees = -20
+
+// if (month === 12 || month <= 2) {
+//     // if (degrees <= -25) {
+//     //     console.log("Зима (холодная зима)")
+//     // } else {
+//     //     console.log("Зима")
+//     // }
+//     degrees <= 25 ? console.log("Зима (холодная зима)") : console.log("Зима")
+
+// } else if (month > 2 && month <= 5) {
+//     if (degrees < 15) {
+//         console.log("Весна (прохладная весна)")
+//     } else {
+//         console.log("Весна")
+//     }
+// } else if (month > 5 && month <= 8) {
+//     if (degrees >= 30) {
+//         console.log("Лето (жаркое лето)")
+//     } else {
+//         console.log("Лето")
+//     }
+// } else if (month > 8 && month <= 11) {
+//     if (degrees < 0) {
+//         console.log("Осень (прохладная осень)")
+//     } else {
+//         console.log("Осень")
+//     }
+// }
+
+// const usernames = ['@user1', '@user2', '@user3']
+
+// for
+// while
+// do while
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+// let a = 0;
+// while (a < 5) {
+//     console.log(a)
+//     a++
+// }
+
+// let b = 6
+// do {
+//     console.log(b)
+// } while (b < 5)
+
+// for (let i = 0; i < usernames.length; i++) {
+//     console.log(usernames[i])
+// }
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+// Вывести все числа от 1 до числа которое ввел пользователь с помощю цикла while
+let num = 10; // это число которое ввел пользователь
+let i = 1; // переменная цикла
+while (i <= num) { // цикл
+    console.log(i) // выводим число
+    i++ // прибавляем переменной 1
+}
+
