1a5f988907df5b7703a32ee5523cd2cebef62f29
--replace-all <visolad1@gmail.com>
2023-12-16 19:08:10 +0500

    Lesson 23, 24

---
+++ Lesson_23/index.js
@@ -0,0 +1,13 @@
+// let mass = []
+// mass.push(1)
+
+// console.log(mass)
+
+let nums = [0, 2, 3, 6]
+nums[0] = 3
+console.log(nums)
+
+nums[0] // получаем элемент по индексу
+nums[0] = 3 // меняем массив 
+nums.push(9) // Добавляем число 9 в массив
+
