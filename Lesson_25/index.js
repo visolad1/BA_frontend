57c6580b13b5ace87324b75da0760580343fdbc7
--replace-all <visolad1@gmail.com>
2023-12-20 23:11:04 +0500

    Lesson 25

---
+++ Lesson_25/index.js
@@ -0,0 +1,45 @@
+// function sum(a, b) { // Обявление фуекции
+//     console.log(a + b)
+// }
+
+// sum(3, 5)
+
+// const privet = (name) => { // Стрелочная функция
+//     console.log(`Privet ${name}`)
+// }
+
+// privet('Shahzoda')
+
+// let a = 'Hello'
+// let b = 'World'
+// let c = a + b
+// console.log(c)
+
+// let nums = [2, 4, 5, 6, 3, 6]
+// let a = 0
+// console.log(nums[a])
+// a = a + 1
+// console.log(nums[a])
+// a = a + 1
+// console.log(nums[a])
+// a = a + 1
+// console.log(nums[a])
+// a = a + 1
+// console.log(nums[a])
+// a = a + 1
+// console.log(nums[a])
+
+
+// for(let a = 0; a < nums.length; a++){
+//     console.log(nums[a])
+// }
+
+
+function sum(a, b) {
+    return a + b ** a + b
+    console.log('hfehrj')
+}
+
+let c = 5
+let result = sum(c, 6)
+console.log(result)
