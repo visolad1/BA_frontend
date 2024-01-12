3c3b12d49cd0f1787eb85cc28ac9512f55916346
--replace-all <visolad1@gmail.com>
2024-01-12 23:34:14 +0500

    Lesson 29

---
+++ Lesson_29/index.js
@@ -0,0 +1,18 @@
+// history;
+// navigator;
+// location;
+
+// window.history.go(+1)
+
+// document.querySelector('selector')
+
+// document.getElementsByClassName('div')
+
+// document.getElementById('hi')
+
+const btn = document.getElementById('hideBtn')
+const text = document.getElementById('hide')
+
+btn.onclick = function () {
+    text.style.display = 'block'
+}
